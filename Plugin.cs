7e7baaf16d8e8816babffd4d9d17fb3d490eb5e8
Benedikt Werner <1benediktwerner@gmail.com>
2022-07-23 11:02:45 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace InfiniteMonths
+{
+    [BepInPlugin("de.benediktwerner.stacklands.infinitemonths", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPatch(typeof(WorldManager), "MonthTime", MethodType.Getter)]
+        [HarmonyPrefix]
+        private static void WorldManager__MonthTime__get__Prefix(ref bool __runOriginal, out float __result)
+        {
+            __runOriginal = false;
+            __result = float.PositiveInfinity;
+        }
+    }
+}
